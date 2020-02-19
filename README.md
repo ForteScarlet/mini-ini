0aa07bb82315961dacdb8c8c4387272676ac25f5
ForteScarlet <ForteScarlet@163.com>
2020-02-19 21:31:03 +0800

    :tada: Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+## What is this?
+This is a mini util for .ini File
---
+++ src/main/java/io/github/miniini/Ini.java
@@ -0,0 +1,14 @@
+package io.github.miniini;
+
+/**
+ *
+ *
+ * @author ForteScralet
+ * @since 1.0
+ */
+public class Ini {
+
+    public void run(){
+    }
+
+}
